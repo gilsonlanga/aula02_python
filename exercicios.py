76c2541103895239da907f586021aa9e9d6baf3b
Gilson Langa <giu.langa@gmail.com>
2025-06-21 10:50:14 -0300

    atualizado exercicios

--- exercicios.py
+++ exercicios.py
@@ -7,10 +7,15 @@ import math
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 
-# numero_01 = int(input("Inserir um número inteiro: "))
-# numero_02 = int(input("Inserir outro número inteiro: "))
-# resultado = numero_01 // numero_02
-# print(resultado)
+try:
+    numero_01 = int(input("Inserir um número inteiro: "))
+    numero_02 = int(input("Inserir outro número inteiro: "))
+    resultado = numero_01 // numero_02
+    print(resultado)
+except ZeroDivisionError:
+    print("integer division or modulo by zero")
+except KeyboardInterrupt:
+    print("acho que você não quis inserir um numero")
 
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
@@ -35,13 +40,11 @@ import math
 # 13. Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 # 14. Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 
-data_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
-lista_dia_mes_ano = data_usuario.split("/")
-print(f"O elemento 1 é o: {lista_dia_mes_ano[0]}")
-print(f"O elemento 2 é o: {lista_dia_mes_ano[1]}")
-print(f"O elemento 3 é o: {lista_dia_mes_ano[2]}")
-
-
+# data_usuario = input("Insira uma data no formato dd/mm/aaaa: ")
+# lista_dia_mes_ano = data_usuario.split("/")
+# print(f"O elemento 1 é o: {lista_dia_mes_ano[0]}")
+# print(f"O elemento 2 é o: {lista_dia_mes_ano[1]}")
+# print(f"O elemento 3 é o: {lista_dia_mes_ano[2]}")
 
 
 # 15. Escreva um programa que concatene duas strings fornecidas pelo usuário.
